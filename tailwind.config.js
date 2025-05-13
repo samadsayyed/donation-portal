c18381d749bbfbda30d8b5925dd86f13aceb3377
samadsayyed <samadsayyed812@gmail.com>
2025-05-13 19:49:12 +0530

    style: update color scheme for better visual consistency
    The color values for `grey`, `customBeige`, `maroon`, `primary`, and `primaryHover` were updated to `#10344c` to ensure a more cohesive and visually appealing design. Additionally, the background color for selected amounts in the `AmountSelection` component was chang

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,11 @@ export default {
   theme: {
     extend: {
       colors: {
-        grey: "#0F172A", // dark navy grey used in the header/nav
-        customBeige: "#0F172A", // vibrant cyan used in buttons and accents
-        maroon: "#0f172a", // deep background used in hero section
-        primary: "#0f172a", // primary teal-blue from buttons
-        primaryHover: "#0F172A", // slightly darker hover for CTA buttons
+        grey: "#10344c", // dark navy grey used in the header/nav
+        customBeige: "#10344c", // vibrant cyan used in buttons and accents
+        maroon: "#10344c", // deep background used in hero section
+        primary: "#10344c", // primary teal-blue from buttons
+        primaryHover: "#10344c", // slightly darker hover for CTA buttons
         text: {
           primary: "#FFFFFF", // white text seen in the header and hero
           secondary: "#E2E8F0", // light greyish text for sub-descriptions
